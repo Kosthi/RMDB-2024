c1a8680c18b79aa25406a0ed322e6f3f44c0e838
Koschei <nitianzero@gmail.com>
2024-05-20 19:51:20 +0800

    feat: implement lru replacer

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -1,7 +1,7 @@
 /* Copyright (c) 2023 Renmin University of China
 RMDB is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
+You can use this software according to the terms and conditions of the Mulan PSL
+v2. You may obtain a copy of Mulan PSL v2 at:
         http://license.coscl.org.cn/MulanPSL2
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
@@ -10,24 +10,33 @@ See the Mulan PSL v2 for more details. */
 
 #include "lru_replacer.h"
 
+#include <unistd.h>
+
 LRUReplacer::LRUReplacer(size_t num_pages) { max_size_ = num_pages; }
 
-LRUReplacer::~LRUReplacer() = default;  
+LRUReplacer::~LRUReplacer() = default;
 
 /**
  * @description: 使用LRU策略删除一个victim frame，并返回该frame的id
- * @param {frame_id_t*} frame_id 被移除的frame的id，如果没有frame被移除返回nullptr
+ * @param {frame_id_t*} frame_id
+ * 被移除的frame的id，如果没有frame被移除返回nullptr
  * @return {bool} 如果成功淘汰了一个页面则返回true，否则返回false
  */
-bool LRUReplacer::victim(frame_id_t* frame_id) {
+bool LRUReplacer::victim(frame_id_t *frame_id) {
     // C++17 std::scoped_lock
     // 它能够避免死锁发生，其构造函数能够自动进行上锁操作，析构函数会对互斥量进行解锁操作，保证线程安全。
-    std::scoped_lock lock{latch_};  //  如果编译报错可以替换成其他lock
+    std::scoped_lock lock{latch_}; //  如果编译报错可以替换成其他lock
 
     // Todo:
     //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
     //  选择合适的frame指定为淘汰页面,赋值给*frame_id
+    if (LRUlist_.empty()) {
+        return false;
+    }
 
+    *frame_id = LRUlist_.back();
+    LRUlist_.pop_back();
+    LRUhash_.erase(*frame_id);
     return true;
 }
 
@@ -40,6 +49,11 @@ void LRUReplacer::pin(frame_id_t frame_id) {
     // Todo:
     // 固定指定id的frame
     // 在数据结构中移除该frame
+    auto &&it = LRUhash_.find(frame_id);
+    if (it != LRUhash_.end()) {
+        LRUlist_.erase(it->second);
+        LRUhash_.erase(it);
+    }
 }
 
 /**
@@ -50,6 +64,17 @@ void LRUReplacer::unpin(frame_id_t frame_id) {
     // Todo:
     //  支持并发锁
     //  选择一个frame取消固定
+    std::scoped_lock lock{latch_};
+
+    // 满了
+    if (LRUlist_.size() >= max_size_) {
+        return;
+    }
+
+    if (LRUhash_.count(frame_id) == 0) {
+        LRUlist_.emplace_front(frame_id);
+        LRUhash_[frame_id] = LRUlist_.begin();
+    }
 }
 
 /**
