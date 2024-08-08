0f54229f5fb6e3af9106438582d117f94ba89163
Koschei <nitianzero@gmail.com>
2024-08-08 18:47:04 +0800

    feat: 尝试加插入和读行锁

--- src/record/rm_file_handle.cpp
+++ src/record/rm_file_handle.cpp
@@ -21,9 +21,9 @@ std::unique_ptr<RmRecord> RmFileHandle::get_record(const Rid &rid, Context *cont
     // 1. 获取指定记录所在的page handle
     // 2. 初始化一个指向RmRecord的指针（赋值其内部的data和size）
     // 行级 S 锁
-    // if (context != nullptr && context->lock_mgr_ != nullptr) {
-    //     context->lock_mgr_->lock_shared_on_record(context->txn_, rid, fd_);
-    // }
+    if (context != nullptr && context->lock_mgr_ != nullptr) {
+        context->lock_mgr_->lock_shared_on_record(context->txn_, rid, fd_);
+    }
     auto &&page_handle = fetch_page_handle(rid.page_no);
     if (!Bitmap::is_set(page_handle.bitmap, rid.slot_no)) {
         throw RecordNotFoundError(rid.page_no, rid.slot_no);
@@ -48,15 +48,15 @@ Rid RmFileHandle::insert_record(char *buf, Context *context) {
     // 注意考虑插入一条记录后页面已满的情况，需要更新file_hdr_.first_free_page_no
     // TODO 不需要加行级写锁？
     // TODO 这里实际上是以一个页面放一个记录，太占用空间了！
-    auto &&page_handle = create_page_handle();
+    auto page_handle = create_page_handle();
     // TODO 算法优化
-    auto &&slot_no = Bitmap::first_bit(false, page_handle.bitmap, file_hdr_.num_records_per_page);
+    auto slot_no = Bitmap::first_bit(false, page_handle.bitmap, file_hdr_.num_records_per_page);
 
     // 行级 X 锁
-    // if (context != nullptr) {
-    //     context->lock_mgr_->lock_exclusive_on_record(context->txn_, {page_handle.page->get_page_id().page_no, slot_no},
-    //                                                  fd_);
-    // }
+    if (context != nullptr) {
+        context->lock_mgr_->lock_exclusive_on_record(context->txn_, {page_handle.page->get_page_id().page_no, slot_no},
+                                                     fd_);
+    }
 
     memcpy(page_handle.get_slot(slot_no), buf, file_hdr_.record_size);
     Bitmap::set(page_handle.bitmap, slot_no);
