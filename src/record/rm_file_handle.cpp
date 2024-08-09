bae01a10535f99ffd25c80b9a52e063ac84206c7
Koschei <nitianzero@gmail.com>
2024-08-10 03:21:53 +0800

    feat: 插入锁

--- src/record/rm_file_handle.cpp
+++ src/record/rm_file_handle.cpp
@@ -56,16 +56,16 @@ Rid RmFileHandle::insert_record(char *buf, Context *context) {
     auto slot_no = Bitmap::first_bit(false, page_handle.bitmap, file_hdr_.num_records_per_page);
 
     // 行级 X 锁
-    // if (context != nullptr) {
-    //     try {
-    //         context->lock_mgr_->lock_exclusive_on_record(context->txn_, {page_handle.page->get_page_id().page_no, slot_no},
-    //                                                      fd_);
-    //     } catch (TransactionAbortException &e) {
-    //         // 这里记得要先解锁，不能会死锁
-    //         page_handle.page->WUnlatch();
-    //         throw e;
-    //     }
-    // }
+    if (context != nullptr) {
+        try {
+            context->lock_mgr_->lock_exclusive_on_record(context->txn_, {page_handle.page->get_page_id().page_no, slot_no},
+                                                         fd_);
+        } catch (TransactionAbortException &e) {
+            // 这里记得要先解锁，不能会死锁
+            page_handle.page->WUnlatch();
+            throw e;
+        }
+    }
 
     // TODO 优化插入
     memcpy(page_handle.get_slot(slot_no), buf, file_hdr_.record_size);
